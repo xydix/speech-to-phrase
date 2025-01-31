43797ea541fa6c5c43d260c7c93c1008b0ba2adb
Michael Hansen <mike@rhasspy.org>
2025-01-31 10:15:06 -0600

    French tests passing

--- tests/test_french.py
+++ tests/test_french.py
@@ -26,12 +26,12 @@ THINGS = Things(
         # Entity(names=["Bed Light"], domain="light"),
         # Entity(names=["Outdoor Humidity"], domain="sensor"),
         # Entity(names=["Garage Door"], domain="cover"),
-        # Entity(names=["Front Door"], domain="lock"),
+        Entity(names=["Porte d'entrée"], domain="lock"),
         # Entity(names=["Party Time"], domain="script"),
         # Entity(names=["Mood Lighting"], domain="scene"),
     ],
-    areas=[Area(names=["Bureau"]), Area(names=["Salon"])],
-    floors=[Floor(names=["Premier Étage"])],
+    areas=[Area(names=["Bureau"]), Area(names=["Salon"]), Area(names=["Chambre"])],
+    floors=[Floor(names=["Premier Étage"]), Floor(names=["Rez-de-chaussée"])],
 )
 
 VAD = SileroVoiceActivityDetector()
@@ -46,39 +46,56 @@ async def train_french() -> None:
     await train(MODEL, SETTINGS, THINGS)
 
 
+# Some of these sentences have incorrect grammar. This is on purpose because:
+# 1. There is no way speech-to-phrase is going to get it exactly correct
+# 2. It doesn't matter anyways because the intent recognizer is flexible
 @pytest.mark.parametrize(
     "text",
     [
-        "quelle heure est-il",
-        "quel jour sommes-nous",
-        "quel temps fait-il",
-        "quel temps fait-il à New York",
+        "allume la Chambre à 80 pourcent",
+        "allume la lumière",
+        "allume le Bureau en rouge",
+        "allume le Premier Étage en vert",
+        "allume le Rez-de-chaussée à 80 pourcent",
+        "allume les lumières dans la Chambre",
+        "allume les lumières en bleu",
+        "allume toutes les lumières",
+        "allume toutes les lumières dans le Rez-de-chaussée",
+        "annuler",
+        "combien de temps reste-t-il",
         "combien fait-il",
+        "combien fait-il dans le Bureau",
         "combien fait-il dans le Salon",
-        # "turn on Standing Light",
-        # "turn off lights in the Office",
-        # "turn on lights on the First Floor",
-        # "set Kitchen lights to green",
-        # "set Bed Light brightness to 50 percent",
-        # "what is the Outdoor Humidity",
-        # "close the Garage Door",
-        # "is the Garage Door open",
-        # "lock the Front Door",
-        # "is the Front Door locked",
-        "minuteur 5 minutes",
+        "déverrouille la Porte d'entrée",
+        "éteins la lumière",
+        "éteins les lumières dans le Salon",
+        "éteins toutes les lumières",
+        "éteins toutes les lumières du Premier Étage",
+        "ferme les rideau du Bureau",
+        "fermer les rideau",
+        "ferme tous les volet",
+        "média suivant",
+        "minuteur 10 minutes",
+        "minuteur 1 heures",
+        "minuteur 20 secondes",
         "minuteur 30 secondes",
         "minuteur 3 heures et 10 minutes",
-        # "pause timer",
-        # "resume timer",
-        # "cancel timer",
-        # "cancel all timers",
-        "combien de temps reste-t-il",
+        "minuteur 5 minutes",
+        "ouvre les rideau du Rez-de-chaussée",
+        "ouvre les volet",
+        "ouvre les volet du Premier Étage",
+        "ouvre tous les volet",
         "pause",
-        # "resume",
+        "quel jour sommes-nous",
+        "quelle heure est-il",
+        "quel temps fait-il",
+        "quel temps fait-il à New York",
+        "règle la luminosité dans le Bureau à 50 pourcent",
+        "silence",
         "suivant",
-        # "run Party Time",
-        # "activate Mood Lighting",
-        # "never mind",
+        "supprime le minuteur",
+        "supprime tous les minuteurs",
+        "verrouille la Porte d'entrée",
     ],
 )
 @pytest.mark.asyncio
@@ -91,12 +108,12 @@ async def test_transcribe(text: str, train_french) -> None:
     assert text == transcript
 
 
-# @pytest.mark.parametrize("wav_num", [1, 2, 3])
-# @pytest.mark.asyncio
-# async def test_oov(wav_num: int, train_english) -> None:
-#     """Test transcribing out-of-vocabulary (OOV) sentences."""
-#     wav_path = WAV_DIR / f"oov_{wav_num}.wav"
-#     assert wav_path.exists(), f"Missing {wav_path}"
+@pytest.mark.parametrize("wav_num", [1, 2, 3, 4])
+@pytest.mark.asyncio
+async def test_oov(wav_num: int, train_french) -> None:
+    """Test transcribing out-of-vocabulary (OOV) sentences."""
+    wav_path = WAV_DIR / f"oov_{wav_num}.wav"
+    assert wav_path.exists(), f"Missing {wav_path}"
 
-#     transcript = await transcribe(MODEL, SETTINGS, wav_audio_stream(wav_path, VAD))
-#     assert not transcript
+    transcript = await transcribe(MODEL, SETTINGS, wav_audio_stream(wav_path, VAD))
+    assert not transcript
